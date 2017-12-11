4b152122ef395236e4503818cc4332446b8becbb
John Turner <john.turner@parivedasolutions.com>
2017-12-11 12:14:29 -0600

    Updated variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,19 @@
 variable "region" {
+  type = "string"
   default = "us-east-1"
 }
 
 variable "org" {
+  type = "string"
   default = "ParivedaSolutions"
 }
 
 variable "org_short" {
+  type = "string"
   default = "par"
 }
 
 variable "domain" {
+  type = "string"
   default = "parivedasolutions.com"
 }
